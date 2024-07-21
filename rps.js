5caa6d81788ef69f2e51445a1142f21fd59b1241
VinVinnieVince <vincent.chan@hotmail.co.uk>
2024-07-21 14:40:41 +0100

    Add psuedocode

--- rps.js
+++ rps.js
@@ -1,1 +1,18 @@
 // rps = Rock paper scissors
+
+//*Pseudocode */
+
+// 1. CPU choice
+    // 1a. Generate random number:  0, 1, 2
+    // 1b. Convert number to choice (Rock, Paper, Scissors)
+// 2. Ask for user input?
+    // 2a. Numbers? vs Choices?
+    // Pros: Numbers are faster ; Cons: May be confusing initially
+// 3. Generate text: 'Rock... Paper... Scissors!'
+// 4. Show CPU choice
+// 5. If
+    // a. Same choice = Show 'draw!'
+    // b. Inferior choice = Show 'CPU wins!'
+    // c. Superior chocie = Show 'you win!'
+// 6. Repeat 2-5
+
