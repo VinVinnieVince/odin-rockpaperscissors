7dce45b068351db0525df99206918e75e115cc27
VinVinnieVince <vincent.chan@hotmail.co.uk>
2024-07-21 14:55:57 +0100

    Add CPU choice
    Also minor change to psuedocode

--- rps.js
+++ rps.js
@@ -2,10 +2,10 @@
 
 //*Pseudocode */
 
-// 1. CPU choice
+// 1. Get CPU choice
     // 1a. Generate random number:  0, 1, 2
     // 1b. Convert number to choice (Rock, Paper, Scissors)
-// 2. Ask for user input?
+// 2. Get Human choice
     // 2a. Numbers? vs Choices?
     // Pros: Numbers are faster ; Cons: May be confusing initially
 // 3. Generate text: 'Rock... Paper... Scissors!'
@@ -16,3 +16,17 @@
     // c. Superior chocie = Show 'you win!'
 // 6. Repeat 2-5
 
+function getCompChoice() {
+    
+    let choice;
+    let num = Math.floor(Math.random() * 3);
+
+    if (num === 0) {
+        choice = 'Rock';
+    } else if (num === 1) {
+        choice = 'Paper';
+    } else {
+        choice = 'Scissors';
+    }
+    return choice
+}
